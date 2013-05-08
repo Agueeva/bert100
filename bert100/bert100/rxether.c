8a911355f4fbadb1e3360849563bdb0937fd7bf4
Your Name <you@example.com>
2013-05-08 23:11:19 +0200

    Worked on Ethernet driver

--- bert100/bert100/rxether.c
+++ bert100/bert100/rxether.c
@@ -2,18 +2,68 @@
  * Renesas RX Ethernet controller driver
  */
 
+#include <string.h>
 #include "iodefine.h"
 #include "types.h"
 #include "rxether.h"
 #include "phy.h"
 #include "interrupt_handlers.h"
+#include "interpreter.h"
+#include "hex.h"
 
-#define NUM_TXBUFS	(2)
+#define RX_DESCR_NUM	(2U)
+#define TX_DESCR_NUM	(4U)
+
+#define EMAC_NUM_RX_BUFS	RX_DESCR_NUM
+#define EMAC_RX_BUFSIZE		(1536) /* Aligned to multiples of 32 Bytes, not necessary ? */
+
+#define RX_DESCR_WP(rxeth)	((rxeth)->rxDescrWp % RX_DESCR_NUM)
+#define RX_DESCR_RP(rxeth)	((rxeth)->rxDescrRp % RX_DESCR_NUM)
+#define TX_DESCR_WP(rxeth)	((rxeth)->txDescrWp % TX_DESCR_NUM)
+#define TX_DESCR_RP(rxeth)	((rxeth)->txDescrRp % TX_DESCR_NUM)
+
 #define EMAC_TC_INT	(UINT32_C(1) << 21)
 #define EMAC_FR_INT	(UINT32_C(1) << 18)
 
+/**
+ ************************************************************************
+ * Status bits in the transmit descriptor
+ ************************************************************************
+ */
+#define TXDS_ACT	(UINT32_C(1) << 31)
+#define TXDS_DLE	(UINT32_C(1) << 30)
+#define TXDS_FP1	(UINT32_C(1) << 29)
+#define TXDS_FP0	(UINT32_C(1) << 28)
+#define TXDS_FE		(UINT32_C(1) << 27)
+#define TXDS_WBI	(UINT32_C(1) << 26)
+
+/*
+ *************************************************************************
+ * Status bits in the receive descriptor
+ *************************************************************************
+ */
+#define RXDS_ACT	(UINT32_C(1) << 31)
+#define	RXDS_DLE	(UINT32_C(1) << 30)
+#define RXDS_FP1	(UINT32_C(1) << 29)
+#define RXDS_FP0	(UINT32_C(1) << 28)
+#define RXDS_FE		(UINT32_C(1) << 27)
+
+typedef struct Descriptor {
+	uint32_t status;
+	uint16_t size;	  /**< Only relevantin receive descriptor */
+	uint16_t bufsize; /**< TX: number of bytes to transmit, RX: Maxium bytes to receive */
+	uint8_t  *bufP;
+	uint32_t padding;
+} Descriptor;
+
 typedef struct RxEth {
-	uint8_t txBuffer[1522 * NUM_TXBUFS] __attribute__((aligned (4)));	
+	Descriptor txDescr[TX_DESCR_NUM] __attribute__((aligned (16))); 
+	Descriptor rxDescr[RX_DESCR_NUM] __attribute__((aligned (16)));
+	uint8_t rxBuf[EMAC_NUM_RX_BUFS * EMAC_RX_BUFSIZE];
+	uint16_t txDescrWp;
+	uint16_t txDescrRp;
+	uint16_t rxDescrWp;
+	uint16_t rxDescrRp;
 } RxEth;
 
 static RxEth gRxEth;
@@ -96,28 +146,86 @@ void Excep_ETHER_EINT(void)
 	} 
 	if(status & EMAC_TC_INT) {
 		EDMAC.EESR.BIT.TC = 1; /* Clear the transmission complete bit */
+		Con_Printf("TCINT\n");
 	}
 }
 
 void 
 RXEth_Transmit(uint8_t *buf,uint16_t len)
 {
-
+	RxEth *re = &gRxEth;
+	Descriptor *txDescr = &re->txDescr[TX_DESCR_WP(re)];
+	txDescr->bufP = buf;
+	txDescr->bufsize = len;
+	txDescr->status |= (TXDS_FP1 | TXDS_FP0 | TXDS_ACT); /* activate Single buffer frame */
+	re->txDescrWp++;
+	if(EDMAC.EDTRR.LONG = 0) {
+		EDMAC.EDTRR.LONG = 1;
+	}
+	SleepMs(200);
 }
+
+/**
+ ***************************************************************************
+ * Initialize the DMA descriptors.
+ ***************************************************************************
+ */
+static void
+RXEth_InitDescriptors(RxEth *re)
+{
+	int i;
+	Descriptor *descr;
+	for(i = 0; i < TX_DESCR_NUM; i++) {
+		descr = &re->txDescr[i];	
+		descr->status = 0;
+	}
+	descr->status |= TXDS_DLE; /* Mark as last Descriptor in the ring */
+	
+	for(i = 0; i < RX_DESCR_NUM; i++) {
+		descr = &re->rxDescr[i];
+		descr->bufsize = EMAC_RX_BUFSIZE;
+		descr->size = 0;
+		descr->status = RXDS_ACT;
+	}
+	descr->status = RXDS_DLE; /* Mark as last descriptor in the ring */
+	
+}
+/**
+ ***********************************************************************************
+ * \fn static int8_t cmd_ethtx(Interp * interp, uint8_t argc, char *argv[])
+ * Shell interface for testing the ethernet transmission
+ ***********************************************************************************
+ */
+static int8_t
+cmd_ethtx(Interp * interp, uint8_t argc, char *argv[])
+{
+	uint16_t i;
+	uint8_t pkt[64] __attribute__((aligned (32)));
+	memset(pkt,0,sizeof(pkt));
+	for(i = 1; i < argc && (i <= array_size(pkt)); i++) {
+		pkt[i - 1] = astrtoi16(argv[i]);
+	}
+	RXEth_Transmit(pkt,64);
+	return 0;
+}
+
+INTERP_CMD(ethtxCmd, "ethtx", cmd_ethtx, "ethtx      # Raw ethernet transmit test command");
 
 void
 RX_EtherInit()
 {
 	volatile uint32_t i;
+	RxEth *re = &gRxEth;
 	RX_EtherSetupIoPortsMII();
 	MSTP(EDMAC) = 0;
-	for(i = 0; i < 10000; i++) {
+	for(i = 0; i < 1000; i++) {
 
 	}
 	EDMAC.EDMR.BIT.SWR = 1;
-	for(i = 0; i < 10000; i++) {
+	for(i = 0; i < 1000; i++) {
 
 	}
+	RXEth_InitDescriptors(re);
 	/* This is a locally assigned unicast address */
 	ETHERC.MAHR = 0x12345678;
 	ETHERC.MALR.LONG = 0x9abc;
@@ -128,17 +236,16 @@ RX_EtherInit()
 	ETHERC.RFLR.LONG = 1522;	/* Standard Ethernet MAXPS + 4 Byte VLAN Header */
 	ETHERC.IPGR.LONG = 0x14;	
 	EDMAC.EESR.LONG = 0x47ff0f9f;	/* Clear all status bits, should not be necessary, cleared by reset */
-	EDMAC.EDMR.BIT.DE = 1;		/* Little endian DMA */	
-	//EDMAC.RDLAR = bla;
-	//EDMAC.TDLAR = blub;
+	EDMAC.EDMR.BIT.DE = 1;		/* Little endian DMA for descriptors */	
+	EDMAC.RDLAR = &re->rxDescr[0]; 
+	EDMAC.TDLAR = &re->txDescr[0];
 	EDMAC.TRSCER.LONG = 0;		/* Should not be necessary because cleared by reset */
 	EDMAC.TFTR.LONG = 0;		/* Store and forward mode */
 	EDMAC.FDR.LONG = 5;		/* Fifo 1536 bytes */
 	EDMAC.RMCR.LONG = 0;		/* Should not be necessary because cleared by reset */
 	EDMAC.FCFTR.LONG = 0x00070005; 
-#if 0
 	IPR(ETHER,EINT) = 2;
-	IEN(ETHER,EINT = 1;
-#endif
+	IEN(ETHER,EINT) = 1;
 	Phy_Init();
+	Interp_RegisterCmd(&ethtxCmd);
 }
