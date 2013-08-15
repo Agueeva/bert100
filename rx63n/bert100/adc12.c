deb1a1c47225dc86351fe923e28d515522cd322c
Your Name <you@example.com>
2013-08-15 23:45:56 +0200

    Changes

--- rx63n/bert100/adc12.c
+++ rx63n/bert100/adc12.c
@@ -9,23 +9,27 @@
 #include "interpreter.h"
 #include "hex.h"
 
-uint16_t ADC12_Read(uint8_t channel)
+uint16_t ADC12_Read(int channel)
 {
     uint16_t adc_value;
+    volatile uint16_t *addr;
     if(channel < 16) {
     	S12AD.ADANS0.WORD = 0x0001 << channel;
     	S12AD.ADANS1.WORD = 0;
-    } else {
+    } else if(channel <= 20) {
     	S12AD.ADANS0.WORD = 0;
     	S12AD.ADANS1.WORD = 0x0001 << (channel - 16);
+    } else {
+	Con_Printf("Illegal A/D channel %lu\n",channel);
+	return 0;
     }	
     /* Start a conversion */
     S12AD.ADCSR.BIT.ADST = 1;
     /* Wait for the conversion to end */
+    addr = &S12AD.ADDR0;
     while(1 == S12AD.ADCSR.BIT.ADST);
-
     /* Fetch ADC value */
-    adc_value = (uint16_t)(S12AD.ADDR0 & 0x0FFF);
+    adc_value = (uint16_t)(addr[channel] & 0x0FFF);
     return adc_value;
 }
 
@@ -47,6 +51,7 @@ cmd_adc12(Interp * interp, uint8_t argc, char *argv[])
 }
 
 INTERP_CMD(adc12Cmd, "adc12", cmd_adc12, "adc12 <channel-nr> # Read from 12 Bit A/D converter");
+
 void
 ADC12_Init(void) 
 {
@@ -54,7 +59,7 @@ ADC12_Init(void)
 	/* ADC clock = PCLK/8, single scan mode */
 	S12AD.ADCSR.BYTE = 0x00;
 	/* Selects AN000 */
-	S12AD.ADANS0.WORD = 0x0001;
+	//S12AD.ADANS0.WORD = 0x0001;
 	//PORT4.PMR.BYTE = 0x01;
 	Interp_RegisterCmd(&adc12Cmd);
 }
--- rx63n/bert100/adc12.h
+++ rx63n/bert100/adc12.h
@@ -1,3 +1,3 @@
 #include "types.h"
 void ADC12_Init(void);
-uint16_t ADC12_Read(uint8_t channel);
+uint16_t ADC12_Read(int channel);
