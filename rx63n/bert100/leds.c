9f4eb85ec6e9167cb0d7ef083e0ca0757b7e2e2d
Your Name <you@example.com>
2013-08-28 17:13:31 +0200

    Added leds file

---
+++ rx63n/bert100/leds.c
@@ -0,0 +1,47 @@
+/**
+ *********************************************************************
+ * This is the LED controling module.
+ *********************************************************************
+ */
+#include "types.h"
+#include "timer.h"
+#include "shiftreg.h"
+#include "hex.h"
+#include "leds.h"
+#include "interpreter.h"
+
+typedef struct LedModule {
+	Timer refreshTimer;
+	uint16_t shiftreg;
+} LedModule;
+
+LedModule gLedModule;
+
+static void 
+LedsRefresh(void *eventData)
+{
+	LedModule *lm = eventData;	
+	ShiftReg_Out(&lm->shiftval)
+}
+
+void
+Led_Set(uint8_t led_nr,bool on) 
+{
+	LedModule *lm = &gLedModule;
+	if(led_nr < 16) {
+		if(on) {
+			lm->shiftreg |= (UINT16_C(1) << led_nr);
+		} else {
+			lm->shiftreg &= ~(UINT16_C(1) << led_nr);
+		}
+		Timer_Start(&lm->refreshTimer,10);
+		return;
+	}
+}
+
+void 
+Leds_Init() 
+{
+	LedModule *lm = &gLedModule;
+	Timer_Init(&lm->refreshTimer,LedsRefresh,lm);
+}
---
+++ rx63n/bert100/leds.h
@@ -0,0 +1,3 @@
+#include "types.h"
+void Leds_Init();
+void Led_Set(uint8_t led_nr,bool on);
