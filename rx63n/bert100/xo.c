61886ec73ff569c692f7fc32b2a08cb5b0ed99ac
Your Name <you@example.com>
2013-08-29 18:18:25 +0200

    Renamde to xo because the 570 is not voltage controlled

---
+++ rx63n/bert100/xo.c
@@ -0,0 +1,40 @@
+/**
+ **********************************************************************************
+ * Interface to the Silabs Si570 Programmable Oscilator XO
+ **********************************************************************************
+ */
+
+#include "types.h"
+#include "pvar.h"
+#include "interpreter.h"
+#include "console.h"
+#include "hex.h"
+
+typedef struct SiXO {
+	uint8_t i2cAddr;
+} SiXO;
+
+static SiXO gSiXO[1];
+
+/**
+ ******************************************************************************
+ * \fn static int8_t cmd_vxco(Interp * interp, uint8_t argc, char *argv[])
+ * Command shell interface for controlling the Silabs Si570 Oscillator 
+ ******************************************************************************
+ */
+
+static int8_t
+cmd_xo(Interp * interp, uint8_t argc, char *argv[])
+{
+	Con_Printf("Not implemented\n");
+        return 0;
+}
+INTERP_CMD(xoCmd,"xo", cmd_xo, "xo  ?<freq>? # Set/Get frequency of XO");
+
+void
+VXCO_Init(const char *name,uint8_t i2cAddr) 
+{
+	SiXO *si = &gSiXO[0];	
+	si->i2cAddr = i2cAddr;	
+	Interp_RegisterCmd(&xoCmd);
+}
