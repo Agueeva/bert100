5ccdf399adbfefd0a2122b1172a03a1ed52f5030
Your Name <you@example.com>
2013-10-08 18:03:50 +0200

    Added variable for reading fan controller rpm

--- rx63n/bert100/fanco.c
+++ rx63n/bert100/fanco.c
@@ -8,6 +8,8 @@
 #include "hex.h"
 #include "i2cmaster.h"
 #include "fanco.h"
+#include "timer.h"
+#include "pvar.h"
 
 #define MAX6651_SPEED       (0)
 #define MAX6651_CONFIG      (2)
@@ -32,8 +34,12 @@
 #define MAX6651_GPIO_STAT   (20)
 #define MAX6651_COUNT       (22)
 
+#define NR_FANS	(4)
+
 typedef struct FanCo {
 	uint16_t i2cAddr;
+	uint16_t fanRpm[NR_FANS];
+	TimeMs_t timeStamp[NR_FANS];
 } FanCo;
 
 static FanCo gFanCo[1];
@@ -42,19 +48,39 @@ static uint32_t
 FanCo_GetRpm(FanCo *fc,int fanNr)
 {
         uint8_t tacho;
+	TimeMs_t now = TimeMs_Get();
         uint32_t rpm;
-	I2C_Read8(fc->i2cAddr,0xc + (fanNr << 1),&tacho, 1);
-	rpm = (((uint32_t)tacho << 1) >> 1) * 60;
+	if(fanNr >= NR_FANS) {
+		return 0;
+	}
+	if((now - fc->timeStamp[fanNr]) > 550) {
+		I2C_Read8(fc->i2cAddr,0xc + (fanNr << 1),&tacho, 1);
+		fc->fanRpm[fanNr] = rpm = (((uint32_t)tacho << 1) >> 1) * 60;
+		fc->timeStamp[fanNr] = now;
+	} else {
+		rpm = fc->fanRpm[fanNr];
+	}
 	return rpm;
+}
+
+static bool
+PVRpm_Get (void *cbData, uint32_t adId, char *bufP,uint16_t maxlen)
+{
+	FanCo *fc = cbData;
+	uint16_t rpm,fanNr;
+	fanNr = adId;
+	rpm = FanCo_GetRpm(fc,fanNr);
+        bufP[uitoa16(rpm, bufP)] = 0;
+	return true;
 }
 
 static int8_t
 cmd_fan(Interp * interp, uint8_t argc, char *argv[])
 {
-	int i;
+	uint32_t i;
 	FanCo *fc = &gFanCo[0];
-	for(i = 0; i < 4; i ++) {
-		Con_Printf("RPM %lu\n",FanCo_GetRpm(fc,i));
+	for(i = 0; i < NR_FANS; i ++) {
+		Con_Printf("Fan %lu RPM %lu\n",i,FanCo_GetRpm(fc,i));
 	}
 	return 0;
 }
@@ -66,7 +92,12 @@ FanCo_Init(void)
 {
 	FanCo *fc = &gFanCo[0];
 	uint8_t value = 1;
+	uint16_t fanNr;
 	fc->i2cAddr = 0x3e;
 	I2C_Write8(fc->i2cAddr,MAX6651_COUNT,&value,1);
 	Interp_RegisterCmd(&fanCmd);
+	for(fanNr = 0 ; fanNr < NR_FANS; fanNr++)
+	{
+		PVar_New(PVRpm_Get,NULL,fc,fanNr ,"fanco.fan%u.rpm",fanNr);
+	}
 }
