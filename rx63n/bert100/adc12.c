cdefbc1a7d2d81c7bdbb362bfebd60d88a191918
work100 <work100@tnlini02.site>
2013-10-15 00:14:42 +0200

    Manipulated temperature sensor

--- rx63n/bert100/adc12.c
+++ rx63n/bert100/adc12.c
@@ -65,6 +65,24 @@ PVAdc12_GetRaw (void *cbData, uint32_t adId, char *bufP,uint16_t maxlen)
 	bufP[cnt] = 0;
 	return true;
 }
+static bool 
+PVAdc12_GetTemperature (void *cbData, uint32_t adId, char *bufP,uint16_t maxlen)
+{
+	uint32_t adval;
+	float temperature;
+    	S12AD.ADANS0.WORD = 0;
+    	S12AD.ADANS1.WORD = 0;
+	S12AD.ADEXICR.WORD = (1 << 8); /* Temperature sensor select */
+	/* Start a conversion */
+	S12AD.ADCSR.BIT.ADST = 1;
+	/* Wait for the conversion to end */
+    	while(1 == S12AD.ADCSR.BIT.ADST);
+	adval = S12AD.ADTSDR;
+	temperature = (((adval * 3301) / 4096 - 1260) / 4.1) + 25 - 13;
+	bufP[f32toa(temperature,bufP,maxlen)] = 0;
+	return true;
+}
+
 
 /**
  ****************************************************************************
@@ -105,7 +123,7 @@ cmd_temperature(Interp * interp, uint8_t argc, char *argv[])
 	/* Wait for the conversion to end */
     	while(1 == S12AD.ADCSR.BIT.ADST);
 	adval = S12AD.ADTSDR;
-	temperature = (((adval * 3300) / 4096 - 1260) / 4.1) + 25;
+	temperature = (((adval * 3300) / 4096 - 1260) / 4.1) + 25 - 13;
 	Con_Printf("adval %u, temp %f\n",adval,temperature);
 	return 0;	
 }
@@ -129,6 +147,7 @@ ADC12_Init(void)
 		ch->channelNr = i;
 		PVar_New(PVAdc12_GetRaw,PVAdc12_SetRaw,ch,i,"adc12.raw%u",i);
 	}
+	PVar_New(PVAdc12_GetTemperature,NULL,adc,0,"system.temp");
 	/* enable the Temperature Sensor */
 	MSTP_TEMPS = 0;
 	TEMPS.TSCR.BIT.TSEN = 1;
