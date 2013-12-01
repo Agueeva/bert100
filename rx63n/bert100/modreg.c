1526fbe9ca577dea5b1d5586e739d17f78aa7965
work100 <work100@tnlini02.site>
2013-12-01 14:12:01 +0100

    Added 20kHz Signal for modulator control

--- rx63n/bert100/modreg.c
+++ rx63n/bert100/modreg.c
@@ -23,26 +23,40 @@
 static void
 enable_modulator_clock(void)
 {
+	MSTP(MTU4) = 0;
 	/* Setup PE5 for MTIO4C/MTIO2B */
 	BSET(5,PORTE.PDR.BYTE)
-	BCLR(5,PORTE.PODR.BYTE)
+	BSET(5,PORTE.PODR.BYTE)
 	MPC.PE5PFS.BYTE = 0x1; /* Switch Pin to MTIO4C mode */
+	MTU.TSTR.BIT.CST4 = 0;
+	MTU.TOER.BIT.OE4C = 1;	/* Enable the output,must be done before TIOR write */
+	PORTE.PMR.BIT.B5 = 1; 
 
-	MSTP(MTU4) = 0;
-	MTU4.TGRA = 1199;
-
+	MTU4.TGRC = 1199;
 	/* PCLK / 1 */
         MTU4.TCR.BIT.TPSC = 0;
         /* Counter Cleared by TGRC match */
         MTU4.TCR.BIT.CCLR = 5;
         /* Toggle at compare match */
-        MTU4.TIORL.BIT.IOC = 5;
-
-	MTU4.TMDR.BIT.MD = 2;   /* PWM mode 1 */
+        MTU4.TIORL.BIT.IOC = 7;
+	MTU4.TMDR.BIT.MD = 0;   /* Normal mode */
+	MTU.TSTR.BIT.CST4 = 1;
 }
+
+static int8_t
+cmd_mod(Interp * interp, uint8_t argc, char *argv[])
+{
+	Con_Printf("PE5: %u\n",PORTE.PIDR.BIT.B5);
+	Con_Printf("TCNT %u\n",MTU4.TCNT);
+	Con_Printf("TGRC %u\n",MTU4.TGRC);
+	return 0;
+}
+
+INTERP_CMD(modCmd, "mod", cmd_mod, "mod");
 
 void
 ModReg_Init(void)
 {
 	enable_modulator_clock();
+	Interp_RegisterCmd(&modCmd);
 }
