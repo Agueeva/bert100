1226fef1d3686db7f918f0ec54834e72375140a1
Your Name <you@example.com>
2014-01-18 22:04:16 +0100

    Added storage for RV1 and RV2 resistors

--- rx63n/bert100/ntc.c
+++ rx63n/bert100/ntc.c
@@ -18,6 +18,10 @@
 #define NTC_EC_OK               (0)
 #define NTC_EC_RANGE            (1)
 
+#define ADCH_NTC_MOD    (12)
+#define ADCH_NTC_AMP    (13)
+
+
 static float gRPreMod = 10000.0;
 static float gRPreAmp = 10000.0;
 
@@ -27,39 +31,39 @@ typedef struct Point {
 } Point;
 
 const Point points[33] = {
-        {150.0, 301.66},
-        {145.0, 334.51},
-        {140.0, 371.79},
-        {135.0, 414.2},
-        {130.0, 462.59},
-        {125.0, 517.94},
-        {120.0, 581.44},
-        {115.0, 654.5},
-        {110.0, 738.81},
-        {105.0, 836.4},
-        {100.0, 949.73},
-        {95.0, 1081.8},
-        {90.0, 1236.2},
-        {85.0, 1417.4},
-        {80.0, 1630.7},
-        {75.0, 1883.0},
-        {70.0, 2182.4},
-        {65.0, 2539.2},
-        {60.0, 2966.3},
-        {55.0, 3479.8},
-        {50.0, 4100.0},
-        {45.0, 4852.5},
-        {40.0, 5770.3},
-        {35.0, 6895},
-        {30.0, 8218},
-        {25.0, 10000},
-        {20.0, 12142},
-        {15.0, 14827},
-        {10.0, 18216},
-        {5.0, 22520},
-        {0.0, 28024},
-        {-50.0, 38452},
-        {-100.0, 55297},
+        {150.0, 0.030166},
+        {145.0, 0.033451},
+        {140.0, 0.037179},
+        {135.0, 0.04142},
+        {130.0, 0.046259},
+        {125.0, 0.051794},
+        {120.0, 0.058144},
+        {115.0, 0.06545},
+        {110.0, 0.073881},
+        {105.0, 0.08364},
+        {100.0, 0.094973},
+        {95.0, 0.10818},
+        {90.0, 0.12362},
+        {85.0, 0.14174},
+        {80.0, 0.16307},
+        {75.0, 0.18830},
+        {70.0, 0.21824},
+        {65.0, 0.25392},
+        {60.0, 0.29663},
+        {55.0, 0.34798},
+        {50.0, 0.41000},
+        {45.0, 0.48525},
+        {40.0, 0.57703},
+        {35.0, 0.6895},
+        {30.0, 0.8218},
+        {25.0, 1.0000},
+        {20.0, 1.2142},
+        {15.0, 1.4827},
+        {10.0, 1.8216},
+        {5.0, 2.2520},
+        {0.0, 2.8024},
+        {-50.0, 3.8452},
+        {-100.0, 5.5297},
 };
 
 /**
@@ -69,13 +73,14 @@ const Point points[33] = {
  ***************************************************************************
  */
 static float 
-NTC_Interpol_10k(float ohm)
+NTC_Interpol(float ohm,float ntcOhm)
 {
         const Point *p, *prev;
         float diff_ohm;
         float diff_temp;
         float temp;
         int i;
+	ohm = ohm / ntcOhm;
         p = prev = NULL;
         for (i = 1; i < array_size(points); i++) {
                 p = &points[i];
@@ -104,9 +109,15 @@ NTC_Read(int16_t adchannel)
 	float Rv;
 	float ohm;
 	Ri = 1000.0;
-	Rv = 10000.0; 
+	if(adchannel == ADCH_NTC_MOD) {
+		Rv = gRPreMod;
+	} else if (adchannel == ADCH_NTC_AMP) {
+		Rv = gRPreAmp;
+	} else {
+		Rv = 10000;
+	}
 	ohm = (4.1 / volt) * Ri - Ri - Rv;
-	return NTC_Interpol_10k(ohm);
+	return NTC_Interpol(ohm,10000);
 }
 
 static int8_t
@@ -114,21 +125,38 @@ cmd_ntc(Interp *interp,uint8_t argc,char *argv[])
 {
 	if((argc == 3) && (strcmp(argv[1],"ohm") == 0)) {
 		float ohm = astrtof32(argv[2]);
-		Con_Printf("Ohm %f, NTC %f\n",ohm,NTC_Interpol_10k(ohm));
+		Con_Printf("Ohm %f, NTC %f\n",ohm,NTC_Interpol(ohm,10000));
+        	return 0;
 	} else if((argc == 3) && (strcmp(argv[1],"volt") == 0)) {
 		float volt = astrtof32(argv[2]);
 		float Ri = 1000.0;
 		float Rv = 10000;
-		float ohm = (3.3 / volt) * Ri - Ri - Rv;
-		Con_Printf("Ohm %f, NTC %f\n",ohm,NTC_Interpol_10k(ohm));
-	} else {
-		Con_Printf("Mod %f C, Amp %f C\n",NTC_Read(12),NTC_Read(13));
+		float ohm = (4.1 / volt) * Ri - Ri - Rv;
+		Con_Printf("Ohm %f, NTC %f\n",ohm,NTC_Interpol(ohm,10000));
+        	return 0;
+	} else if((argc == 2) && (strcmp(argv[1],"RV1") == 0)) {
+		Con_Printf("RV1(Amp): %f Ohm\n",gRPreAmp - 5100);
+		return 0;
+	} else if((argc == 2) && (strcmp(argv[1],"RV2") == 0)) {
+		Con_Printf("RV2(Amp): %f Ohm\n",gRPreMod - 5100);
+		return 0;
+	} else if((argc == 3) && (strcmp(argv[1],"RV1") == 0)) {
+		gRPreAmp = astrtof32(argv[2]) + 5100;
+		DB_VarWrite(DBKEY_NTC_AMP_RPRE,&gRPreAmp);
+        	return 0;
+	} else if((argc == 3) && (strcmp(argv[1],"RV2") == 0)) {
+		gRPreMod = astrtof32(argv[2]) + 5100;
+		DB_VarWrite(DBKEY_NTC_MOD_RPRE,&gRPreMod);
+        	return 0;
+	} else if(argc == 1) {
+		Con_Printf("Mod %f C, Amp %f C\n",NTC_Read(ADCH_NTC_MOD),NTC_Read(ADCH_NTC_AMP));
+        	return 0;
 	}
-        return 0;
+        return -EC_BADARG;
 }
 
 INTERP_CMD(ntcCmd, "ntc", cmd_ntc,
-           "ntc ohm <value> ");
+           "ntc ?RV1 | RV2? ?<value/Ohm>? ");
 
 static bool
 PVNTC_GetTemperature (void *cbData, uint32_t adchannel, char *bufP,uint16_t maxlen)
@@ -138,6 +166,21 @@ PVNTC_GetTemperature (void *cbData, uint32_t adchannel, char *bufP,uint16_t maxl
         bufP[f32toa(temperature,bufP,maxlen)] = 0;
         return true;
 }
+
+#if 0
+static bool
+PVNTC_GetRV (void *cbData, uint32_t rvX, char *bufP,uint16_t maxlen)
+{
+        float val;
+	if(rvX == 1) {
+		val = gRPreAmp;
+	} else if(rvX == 2) {
+		val = gRPreMod;
+	}
+        bufP[f32toa(temperature,bufP,maxlen)] = 0;
+        return true;
+}
+#endif
 
 void 
 NTC_Init(void) 
