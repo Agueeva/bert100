8189380183ee0c4cd3cb5b69db2f773b85de6599
Your Name <you@example.com>
2014-02-13 19:11:24 +0100

    Added latched alarm pvar

--- rx63n/bert100/alarms.c
+++ rx63n/bert100/alarms.c
@@ -9,21 +9,22 @@
 #include "console.h"
 #include "hex.h"
 #include "pvar.h"
+#include "hex.h"
 
-typedef struct Buzzer {
+typedef struct Alarms {
 	uint32_t alarmBits;
 	uint32_t latchedAlarmBits;
-} Buzzer;
+} Alarms;
 
-static Buzzer gBuzzer;
+static Alarms gAlarms;
 
 void 
 Alarm_Set(uint8_t alarmNr)
 {
 	uint32_t bitmask = (UINT32_C(1) << alarmNr);
-	if(!(gBuzzer.alarmBits & bitmask)) {
-		gBuzzer.alarmBits |= bitmask; 
-		gBuzzer.latchedAlarmBits |= bitmask; 
+	if(!(gAlarms.alarmBits & bitmask)) {
+		gAlarms.alarmBits |= bitmask; 
+		gAlarms.latchedAlarmBits |= bitmask; 
 		Buzzer_Start(2100);
 	}
 }
@@ -32,9 +33,9 @@ void
 Alarm_Clear(uint8_t alarmNr)
 {
 	uint32_t bitmask = (UINT32_C(1) << alarmNr);
-	if(gBuzzer.alarmBits & bitmask) {
-		gBuzzer.alarmBits &= ~(UINT32_C(1) << alarmNr);
-		if(gBuzzer.alarmBits == 0) {
+	if(gAlarms.alarmBits & bitmask) {
+		gAlarms.alarmBits &= ~(UINT32_C(1) << alarmNr);
+		if(gAlarms.alarmBits == 0) {
 			Buzzer_Start(0);
 		}
 	}
@@ -43,20 +44,37 @@ Alarm_Clear(uint8_t alarmNr)
 static bool
 PVAlarms_Get (void *cbData, uint32_t adId, char *bufP,uint16_t maxlen)
 {
-	Buzzer *bz = cbData;
-        SNPrintf(bufP,maxlen,"%lu",bz->alarmBits);
+	Alarms *al = cbData;
+        SNPrintf(bufP,maxlen,"%lu",al->alarmBits);
         return true;
 }
 
+static bool
+PVLatchedAlarms_Get (void *cbData, uint32_t adId, char *bufP,uint16_t maxlen)
+{
+	Alarms *al = cbData;
+        SNPrintf(bufP,maxlen,"%lu",al->latchedAlarmBits);
+        return true;
+}
+
+static bool
+PVLatchedAlarms_Set(void *cbData, uint32_t adId, const char *strP)
+{
+	Alarms *al = cbData;
+	uint32_t value = astrtoi32(strP);
+	al->latchedAlarmBits = value; 
+	al->latchedAlarmBits |= al->alarmBits;
+        return true;
+}
 
 static int8_t
 cmd_alarm(Interp *interp,uint8_t argc,char *argv[])
 {
-	Buzzer *bz = &gBuzzer; 
-	Con_Printf("Alarms         0x%08lx\n",bz->alarmBits);
-	Con_Printf("Latched Alarms 0x%08lx\n",bz->latchedAlarmBits);
+	Alarms *al = &gAlarms; 
+	Con_Printf("Alarms         0x%08lx\n",al->alarmBits);
+	Con_Printf("Latched Alarms 0x%08lx\n",al->latchedAlarmBits);
 	if((argc > 1) && (strcmp(argv[2],"clear") == 0)) {
-		bz->latchedAlarmBits = 0;
+		al->latchedAlarmBits = 0;
 	}
 	return 0;
 }
@@ -67,8 +85,9 @@ INTERP_CMD(alarmCmd, "alarm", cmd_alarm,
 void
 Alarm_Init(void) 
 {
-	Buzzer *bz = &gBuzzer;
-	bz->alarmBits = 0;
+	Alarms *al = &gAlarms;
+	al->alarmBits = 0;
 	Interp_RegisterCmd(&alarmCmd);	
-	PVar_New(PVAlarms_Get,NULL,bz,0,"system.fault");
+	PVar_New(PVAlarms_Get,NULL,al,0,"system.fault");
+	PVar_New(PVLatchedAlarms_Get,PVLatchedAlarms_Set,al,0,"system.latchedFault");
 }
